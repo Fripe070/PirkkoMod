19ed8b458af883b81f85b612c0363041e79fb2a2
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-10-07 22:48:39 +0200

    Working pirkko block

--- src/main/java/io/github/fripe070/pirkko/block/PirkkoBlock.java
+++ src/main/java/io/github/fripe070/pirkko/block/PirkkoBlock.java
@@ -1,36 +1,44 @@
 package io.github.fripe070.pirkko.block;
 
 import com.mojang.serialization.MapCodec;
-
 import eu.pb4.polymer.blocks.api.BlockModelType;
-import eu.pb4.polymer.blocks.api.PolymerBlockModel;
 import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
+import eu.pb4.polymer.virtualentity.api.BlockWithElementHolder;
+import eu.pb4.polymer.virtualentity.api.ElementHolder;
+import eu.pb4.polymer.virtualentity.api.elements.ItemDisplayElement;
+import io.github.fripe070.pirkko.Pirkko;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.WallMountedBlock;
 import net.minecraft.block.enums.BlockFace;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemDisplayContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
 import xyz.nucleoid.packettweaker.PacketContext;
 
-import static io.github.fripe070.pirkko.Pirkko.MOD_ID;
+import java.util.Locale;
 
 
 // TODO: Allow waterlogging
-public class PirkkoBlock extends WallMountedBlock implements PolymerTexturedBlock {
+public class PirkkoBlock extends WallMountedBlock implements PolymerTexturedBlock, BlockWithElementHolder {
     public static final MapCodec<PirkkoBlock> CODEC = createCodec(PirkkoBlock::new);
-
-    private final BlockState polymerBlockState;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+    // TODO: Support more granular rotation
+//    public static final IntProperty ROTATION = Properties.ROTATION;
 
     @Override
     public MapCodec<PirkkoBlock> getCodec() {
@@ -42,36 +50,48 @@ public class PirkkoBlock extends WallMountedBlock implements PolymerTexturedBloc
         this.setDefaultState(this.stateManager.getDefaultState()
             .with(FACING, Direction.NORTH)
             .with(FACE, BlockFace.WALL)
+            .with(WATERLOGGED, false)
         );
-
-        this.polymerBlockState = PolymerBlockResourceUtils.requestBlock(
-            BlockModelType.TRIPWIRE_BLOCK_FLAT,
-            PolymerBlockModel.of(Identifier.of(MOD_ID, "block/pirkko_small")));
     }
 
     @Override
     public BlockState getPolymerBlockState(BlockState state, PacketContext context) {
-        return this.polymerBlockState;
+        return PolymerBlockResourceUtils.requestEmpty(BlockModelType.valueOf((switch (state.get(FACE)) {
+            case FLOOR -> "BOTTOM_TRAPDOOR";
+            case CEILING -> "TOP_TRAPDOOR";
+            default -> state.get(FACING).asString().toUpperCase(Locale.ROOT) + "_TRAPDOOR";
+        }) + (state.get(WATERLOGGED) ? "_WATERLOGGED" : "")));
     }
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        // TODO: Play noise
+        world.playSound(null, pos, SoundEvents.ENTITY_COD_FLOP, SoundCategory.BLOCKS, 3.0f, 1.0f);
         return ActionResult.SUCCESS;
     }
 
     @Override
-    protected BlockState rotate(BlockState state, BlockRotation rotation) {
-        return state.with(FACING, rotation.rotate(state.get(FACING)));
-    }
-
-    @Override
-    protected BlockState mirror(BlockState state, BlockMirror mirror) {
-        return state.rotate(mirror.getRotation(state.get(FACING)));
-    }
-
-    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING, FACE);
+        builder.add(FACING, FACE, WATERLOGGED);
+    }
+
+    @SuppressWarnings("removal")
+    @Override
+    public @Nullable ElementHolder createElementHolder(ServerWorld world, BlockPos pos, BlockState initialBlockState) {
+        var holder = new ElementHolder();
+        ItemDisplayElement element = holder.addElement(new ItemDisplayElement(Pirkko.PIRKKO_BLOCK.asItem()));
+        element.setModelTransformation(ItemDisplayContext.FIXED);
+
+        element.setTransformation(new Matrix4f()
+            .rotate(initialBlockState.get(FACING).getRotationQuaternion())
+            .rotateZ((float) Math.toRadians(180))
+            .rotateX((float) Math.toRadians(switch (initialBlockState.get(FACE)) {
+                case FLOOR -> - 90;
+                case CEILING -> 90;
+                case WALL -> 0;
+            }))
+            .translate(0.0f, - 0.25f, 0.0f)
+            .scale(0.5f)
+        );
+        return holder;
     }
 }
