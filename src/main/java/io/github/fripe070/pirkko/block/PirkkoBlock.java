cf05b94109ca65d2bddf4abe95e2f5d673f8fedb
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-10-08 20:35:25 +0200

    Make pirkko waterloggable

--- src/main/java/io/github/fripe070/pirkko/block/PirkkoBlock.java
+++ src/main/java/io/github/fripe070/pirkko/block/PirkkoBlock.java
@@ -12,9 +12,13 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.WallMountedBlock;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.block.enums.BlockFace;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemDisplayContext;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -25,8 +29,10 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 import xyz.nucleoid.packettweaker.PacketContext;
@@ -35,7 +41,7 @@ import java.util.Locale;
 
 
 // TODO: Allow waterlogging
-public class PirkkoBlock extends WallMountedBlock implements BlockWithElementHolder, PolymerTexturedBlock {
+public class PirkkoBlock extends WallMountedBlock implements BlockWithElementHolder, PolymerTexturedBlock, Waterloggable {
     public static final MapCodec<PirkkoBlock> CODEC = createCodec(PirkkoBlock::new);
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
     // TODO: Support more granular rotation
@@ -56,23 +62,40 @@ public class PirkkoBlock extends WallMountedBlock implements BlockWithElementHol
     }
 
     @Override
-    public BlockState getPolymerBlockState(BlockState state, PacketContext context) {
-        return PolymerBlockResourceUtils.requestEmpty(BlockModelType.valueOf((switch (state.get(FACE)) {
-            case FLOOR -> "BOTTOM_TRAPDOOR";
-            case CEILING -> "TOP_TRAPDOOR";
-            default -> state.get(FACING).asString().toUpperCase(Locale.ROOT) + "_TRAPDOOR";
-        }) + (state.get(WATERLOGGED) ? "_WATERLOGGED" : "")));
-    }
-
-    @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         world.playSound(null, pos, SoundEvents.ENTITY_COD_FLOP, SoundCategory.BLOCKS, 3.0f, 1.0f);
         return ActionResult.SUCCESS;
     }
 
     @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        return super.getPlacementState(ctx)
+            .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+        if (state.get(WATERLOGGED)) tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+    }
+
+    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING, FACE, WATERLOGGED);
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState state, PacketContext context) {
+        return PolymerBlockResourceUtils.requestEmpty(BlockModelType.valueOf((switch (state.get(FACE)) {
+            case FLOOR -> "BOTTOM_TRAPDOOR";
+            case CEILING -> "TOP_TRAPDOOR";
+            default -> state.get(FACING).asString().toUpperCase(Locale.ROOT) + "_TRAPDOOR";
+        }) + (state.get(WATERLOGGED) ? "_WATERLOGGED" : "")));
     }
 
     @SuppressWarnings("removal")
